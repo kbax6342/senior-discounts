94b0b1e60fa42e749f973e25fbc60c1cee6c2ac4
kbax6342 <k3vin.baxt3r@gmail.com>
2023-01-17 07:36:38 -0500

    changed adsense

--- src/components/Ad1/Ad1.js
+++ src/components/Ad1/Ad1.js
@@ -1,22 +1,24 @@
-import React, { Component, useEffect } from 'react'
+import React from 'react'
 
-export default class AdComponent extends Component {
-    componentDidMount () {
+export default function Ad1() {
+  useEffect(() => {
+
+    try{
       (window.adsbygoogle = window.adsbygoogle || []).push({});
     }
-  
-  render () {
-      return (
-      <div>
-        <ins class="adsbygoogle"
-        style={{display:'block'}}
-        data-ad-client="ca-pub-9604401375033815"
-        data-ad-slot="9213937627"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      
-      </div>
-        
-      );
+    catch(e){
+
     }
-  }
+  }, [])
+  
+  return (
+    <div>
+      <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-9604401375033815"
+     data-ad-slot="9213937627"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+    </div>
+  )
+}
